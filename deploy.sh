0c7cf1574f469458c71efc58075bef82bf9ff030
maanex <andreas@maanex.me>
2021-08-01 15:20:00 +0200

    production settings

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 
 echo "Creating public network"
 docker network create --driver=overlay traefik-public
+docker network create --driver=overlay logging
 
 #
 
@@ -11,4 +12,4 @@ echo "Deploying Microservices"
 docker stack deploy -c docker-compose-micros.yml micros
 
 echo "Deploying Apps"
-docker stack deploy -c docker-compose-apps.yml apps
+docker stack deploy -c docker-compose-apps.yml apps --with-registry-auth

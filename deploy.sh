654ae39bbd97eac8202c0ad925985ac98643ca23
maanex <andreas@maanex.me>
2021-08-01 15:22:43 +0200

    add swarmpit to deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,9 @@ docker network create --driver=overlay logging
 echo "Deploying Infrastructure"
 docker stack deploy -c docker-compose-infra.yml infra
 
+echo "Deploying Swarmpit"
+docker stack deploy -c docker-compose-swarmpit.yml swarmpit
+
 echo "Deploying Microservices"
 docker stack deploy -c docker-compose-micros.yml micros
 

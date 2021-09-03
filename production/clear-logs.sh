e5c73013cb21838804f5dae41eafd2fe165c79b1
maanex <andreas@maanex.me>
2021-09-03 18:50:39 +0200

    update cleanup script

--- production/clear-logs.sh
+++ production/clear-logs.sh
@@ -1,2 +1,8 @@
+echo ":: Delete unused containers"
 truncate -s 0 /var/lib/docker/containers/*/*-json.log
+
+echo ":: Delete log files"
+find /var/lib/docker/containers/ -type f -name "*.log" -delete
+
+echo ":: Delete unused images"
 docker system prune -a -f

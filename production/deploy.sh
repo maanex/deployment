5439d0f7afd4fa9f5882ead3acee76d6d605d5de
maanex <andreas@maanex.me>
2022-11-21 11:10:49 +0100

    test

--- production/deploy.sh
+++ production/deploy.sh
@@ -1,30 +1,7 @@
+export $(grep -v '^#' .env | xargs)
 
-echo ":: Creating public network"
-docker network create --driver=overlay proxy
-docker network create --driver=overlay logging
-docker network create --driver=overlay redis
-docker network create --driver=overlay rabbits
-docker network create --driver=overlay fsm-sockets
-docker network create --driver=overlay fsm-micros
-docker network create --driver=overlay metrics
-docker network create --driver=overlay gibu
+$name=$0
 
-#
-
-echo ":: Deploying Infrastructure"
-docker stack deploy -c docker-compose-infra.yml infra
-
-echo ":: Deploying Swarmpit"
-docker stack deploy -c docker-compose-swarmpit.yml swarmpit
-
-echo ":: Deploying Microservices"
-docker stack deploy -c docker-compose-micros.yml micros
-
-echo ":: Deploying Apps"
-docker stack deploy -c docker-compose-apps.yml apps --with-registry-auth
-
-echo ":: Deploying Tude Stack"
-docker stack deploy -c docker-compose-tude.yml tude --with-registry-auth
-
-echo ":: Deploying Greenlight"
-docker stack deploy -c docker-compose-greenlight.yml greenlight --with-registry-auth
+echo ":: Deploying $name"
+# docker stack deploy -c cd-$name.yaml $name --with-registry-auth
+echo $CODER_POSTGRES_PASSWORD
--- production/init.sh
+++ production/init.sh
@@ -2,3 +2,15 @@
 mkdir /var/www/html/data
 touch /var/www/html/data/htaccesstest.txt
 chmod +rw /var/www/html/data/htaccesstest.txt
+
+
+
+echo ":: Creating public network"
+docker network create --driver=overlay proxy
+docker network create --driver=overlay logging
+docker network create --driver=overlay redis
+docker network create --driver=overlay rabbits
+docker network create --driver=overlay fsm-sockets
+docker network create --driver=overlay fsm-micros
+docker network create --driver=overlay metrics
+docker network create --driver=overlay gibu

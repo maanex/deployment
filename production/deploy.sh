f60a22f0b5b68bafd97967cf537c7e45bc7649b9
maanex <andreas@maanex.me>
2022-11-21 11:16:08 +0100

    test6

--- production/deploy.sh
+++ production/deploy.sh
@@ -2,8 +2,7 @@
 
 export $(grep -v '^#' .env | xargs)
 
-name=$0
+name=$1
 
 echo ":: Deploying $name"
-# docker stack deploy -c cd-$name.yaml $name --with-registry-auth
-echo $CODER_POSTGRES_PASSWORD
+docker stack deploy -c cd-$name.yaml $name --with-registry-auth

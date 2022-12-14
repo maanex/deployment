0a5865923c235de2bd4982baf2fab91022265283
maanex <andreas@maanex.me>
2022-12-14 12:50:05 +0100

    ubuntu

--- production/coder/templates/ubuntu-service/README.md
+++
@@ -1,9 +0,0 @@
-# Tude Ubuntu Workspace
-
-This template sets you up with a lightweight ubuntu machine.
-
-You can pick between different base images, some of them have additional software pre-installed.
-
-## Root user
-
-While your default user is `coder`, you can switch to root using `sudo su root` to install additional software.
--- production/coder/templates/ubuntu/main.tf
+++ production/coder/templates/ubuntu/main.tf
@@ -21,6 +21,10 @@ provider "docker" {
 data "coder_workspace" "me" {
 }
 
+data "docker_network" "devenv" {
+  name = "devenv"
+}
+
 resource "coder_agent" "main" {
   arch           = data.coder_provisioner.me.arch
   os             = "linux"
@@ -52,19 +56,11 @@ resource "coder_app" "code-server" {
 
 variable "docker_image" {
   description = "What Docker image would you like to use for your workspace?"
-  default     = "default"
+  default     = "ubuntu"
 
-  # List of images available for the user to choose from.
-  # Delete this condition to give users free text input.
   validation {
-    condition     = contains(["default"], var.docker_image)
+    condition     = contains(["ubuntu"], var.docker_image)
     error_message = "Invalid Docker image!"
-  }
-
-  # Prevents admin errors when the image is not found
-  validation {
-    condition     = fileexists("images/${var.docker_image}.Dockerfile")
-    error_message = "Invalid Docker image. The file does not exist in the images directory."
   }
 }
 
@@ -87,45 +83,65 @@ resource "docker_volume" "home_volume" {
     label = "coder.workspace_id"
     value = data.coder_workspace.me.id
   }
-  # This field becomes outdated if the workspace is renamed but can
-  # be useful for debugging or cleaning out dangling volumes.
   labels {
     label = "coder.workspace_name_at_creation"
     value = data.coder_workspace.me.name
   }
 }
 
-resource "docker_image" "coder_image" {
-  name = "coder-base-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
-  build {
-    path       = "./images/"
-    dockerfile = "${var.docker_image}.Dockerfile"
-    tag        = ["coder-${var.docker_image}:v0.1"]
-  }
-
-  # Keep alive for other workspaces to use upon deletion
-  keep_locally = true
-}
-
-resource "docker_container" "workspace" {
-  count = data.coder_workspace.me.start_count
-  image = docker_image.coder_image.latest
-  # Uses lower() to avoid Docker restriction on container names.
+resource "docker_service" "workspace" {
   name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
-  # Hostname makes the shell more user friendly: coder@my-workspace:~$
-  hostname = data.coder_workspace.me.name
-  # Use the docker gateway if the access URL is 127.0.0.1
-  entrypoint = ["sh", "-c", replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")]
-  env        = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
-  host {
-    host = "host.docker.internal"
-    ip   = "host-gateway"
+  count = data.coder_workspace.me.start_count
+  
+  task_spec {
+    container_spec {
+      image = "ghcr.io/maanex/coder-${var.docker_image}:master"
+
+      command  = ["sh", "-c", replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")]
+      hostname = data.coder_workspace.me.name
+
+      env = {
+        CODER_AGENT_TOKEN = coder_agent.main.token
+      }
+
+      mounts {
+        target    = "/home/coder/"
+        source    = docker_volume.home_volume.name
+        read_only = false
+        type      = "volume"
+      }
+
+      hosts {
+        host = "host.docker.internal"
+        ip   = "host-gateway"
+      }
+
+      # Add labels in Docker to keep track of orphan resources.
+      labels {
+        label = "coder.owner"
+        value = data.coder_workspace.me.owner
+      }
+      labels {
+        label = "coder.owner_id"
+        value = data.coder_workspace.me.owner_id
+      }
+      labels {
+        label = "coder.workspace_id"
+        value = data.coder_workspace.me.id
+      }
+      labels {
+        label = "coder.workspace_name"
+        value = data.coder_workspace.me.name
+      }
+    }
+
+    placement {
+      constraints = [ "node.hostname == co2" ]
+    }
+
+    networks = [ data.docker_network.devenv.id ]
   }
-  volumes {
-    container_path = "/home/coder/"
-    volume_name    = docker_volume.home_volume.name
-    read_only      = false
-  }
+
   # Add labels in Docker to keep track of orphan resources.
   labels {
     label = "coder.owner"
@@ -147,7 +163,7 @@ resource "docker_container" "workspace" {
 
 resource "coder_metadata" "container_info" {
   count       = data.coder_workspace.me.start_count
-  resource_id = docker_container.workspace[0].id
+  resource_id = docker_service.workspace[0].id
 
   item {
     key   = "image"
